ca48f4e8b8ce2826846e1e9ca7108acc98448ac3
Brunin10 <166250771+Brunin10@users.noreply.github.com>
2024-08-15 19:52:29 -0300

    gfghh

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ mostraPergunta () ;
         for (const alternativa of perguntaAtual.alternativas){
             const botaoAlternativas = document.createElement("button") ;
             botaoAlternativas.textContent = alternativa.texto ;
-            botaoAlternativas.addEventListener("click", function(){
+            botaoAlternativas.addEventListener("click", ()=>{
                 pergAtual++;
             mostraPergunta();
         });
